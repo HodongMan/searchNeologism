2d92a464e1e62aeb7a8a405800c5898e72316302
HodongMan <arshavin3@naver.com>
2017-02-04 16:44:30 +0900

    search ranking update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,11 @@ import "./Navbar.css";
 
 const Navbar = () => (
   <nav id="mainNav" className="navbar navbar-default navbar-fixed-top navbar-custom">
-    <div className="container">
-      <h2>LOGO</h2>
-    </div>
+      <div className="container">
+          <h2>LOGO</h2>
+      </div>
   </nav>
+
 );
 
 export default Navbar;
--- src/components/SearchRank/SearchRank.js
+++ src/components/SearchRank/SearchRank.js
@@ -1,77 +1,118 @@
 import React from "react";
+import "./SearchRank.css";
 
 const SearchRank = () => (
-<section id="portfolio">
-  <div className="container">
-    <div className="row">
-      <div className="col-lg-12 text-center">
-        <h2>실시간 검색 순위</h2>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-sm-4 portfolio-item">
-        <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
-          <div className="caption">
-            <div className="caption-content">
-              <i className="fa fa-search-plus fa-3x"></i>
+  <section id="search-list">
+      <div className="container">
+          <div className="row">
+              <div className="col-lg-12 text-center">
+                  <img src="img/rank_title.png" alt="실시간 검색 순위" title="실시간 검색 순위"/>
+                  <p className="search-list-assistant">신조어를 누르면 뜻을 알 수 있어요!</p>
               </div>
-            </div>
-          <img src="img/portfolio/cabin.png" className="img-responsive" alt="" />
-        </a>
-      </div>
-      <div className="col-sm-4 portfolio-item">
-        <a href="#portfolioModal2" className="portfolio-link" data-toggle="modal">
-          <div className="caption">
-            <div className="caption-content">
-              <i className="fa fa-search-plus fa-3x"></i>
-            </div>
           </div>
-          <img src="img/portfolio/cake.png" className="img-responsive" alt=""/>
-        </a>
-      </div>
-      <div className="col-sm-4 portfolio-item">
-        <a href="#portfolioModal3" className="portfolio-link" data-toggle="modal">
-          <div className="caption">
-            <div className="caption-content">
-              <i className="fa fa-search-plus fa-3x"></i>
-            </div>
+          <div className="row search-list-in">
+              <div>
+                  <div className="col-md-6 portfolio-item">
+                      <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                          <div className="search-item">
+                              <div className="search-item-index">1</div>
+                              <div className="search-item-content">
+                                  <div className="search-item-text">
+                                      <span>ㅇㅈ</span>
+                                  </div>
+                                  <div className="search-item-lanked">
+                                      <img src="img/up.png" alt="up" title="up"/>
+                                      <span className="red">1</span>
+                                  </div>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div className="col-md-6 portfolio-item">
+                      <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                          <div className="search-item">
+                              <div className="search-item-index">2</div>
+                              <div className="search-item-content">
+                                  <div className="search-item-text">
+                                      <span>사바사</span>
+                                  </div>
+                                  <div className="search-item-lanked">
+                                      <img src="img/down.png" alt="down" title="down"/>
+                                      <span>1</span>
+                                  </div>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div className="col-md-6 portfolio-item">
+                      <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                          <div className="search-item">
+                              <div className="search-item-index">3</div>
+                              <div className="search-item-content">
+                                  <div className="search-item-text">
+                                      <span>빼박캔트</span>
+                                  </div>
+                                  <div className="search-item-lanked">
+                                      <img src="img/down.png" alt="down" title="down"/>
+                                      <span>1</span>
+                                  </div>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div className="col-md-6 portfolio-item">
+                      <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                          <div className="search-item">
+                              <div className="search-item-index">4</div>
+                              <div className="search-item-content">
+                                  <div className="search-item-text">
+                                      <span>시강</span>
+                                  </div>
+                                  <div className="search-item-lanked">
+                                      <img src="img/none.png" alt="none" title="none"/>
+                                      <span className="gray">0</span>
+                                  </div>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div className="col-md-6 portfolio-item">
+                      <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                          <div className="search-item">
+                              <div className="search-item-index">5</div>
+                              <div className="search-item-content">
+                                  <div className="search-item-text">
+                                      <span>커엽다</span>
+                                  </div>
+                                  <div className="search-item-lanked">
+                                      <img src="img/down.png" alt="down" title="down"/>
+                                      <span>1</span>
+                                  </div>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div className="col-md-6 portfolio-item">
+                      <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                          <div className="search-item">
+                              <div className="search-item-index">6</div>
+                              <div className="search-item-content">
+                                  <div className="search-item-text">
+                                      <span>버카</span>
+                                  </div>
+                                  <div className="search-item-lanked">
+                                      <img src="img/down.png" alt="down" title="down"/>
+                                      <span>1</span>
+                                  </div>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+
           </div>
-          <img src="img/portfolio/circus.png" className="img-responsive" alt=""/>
-        </a>
       </div>
-      <div className="col-sm-4 portfolio-item">
-        <a href="#portfolioModal4" className="portfolio-link" data-toggle="modal">
-          <div className="caption">
-            <div className="caption-content">
-              <i className="fa fa-search-plus fa-3x"></i>
-            </div>
-          </div>
-          <img src="img/portfolio/game.png" className="img-responsive" alt=""/>
-        </a>
-      </div>
-      <div className="col-sm-4 portfolio-item">
-        <a href="#portfolioModal5" className="portfolio-link" data-toggle="modal">
-          <div className="caption">
-            <div className="caption-content">
-              <i className="fa fa-search-plus fa-3x"></i>
-            </div>
-          </div>
-          <img src="img/portfolio/safe.png" className="img-responsive" alt=""/>
-        </a>
-      </div>
-      <div className="col-sm-4 portfolio-item">
-        <a href="#portfolioModal6" className="portfolio-link" data-toggle="modal">
-          <div className="caption">
-            <div className="caption-content">
-              <i className="fa fa-search-plus fa-3x"></i>
-            </div>
-          </div>
-          <img src="img/portfolio/submarine.png" className="img-responsive" alt=""/>
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
+  </section>
 
 );
 
